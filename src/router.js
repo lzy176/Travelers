56c486b662cdfed587939a5aac30185014a084a0
lzy176 <17600273758@163.com>
2024-08-09 15:37:35 +0800

    update

--- src/router.js
+++ src/router.js
@@ -5,7 +5,11 @@ const routes = [
     path: '/',
     redirect: '/home' // 将根路径重定向到 /poetry
   },
-  { path: '/home', component: () => import('@/page/home.vue'), meta: { title: '首页' } },
+  {
+    path: '/home',
+    component: () => import('@/page/home.vue'),
+    meta: { title: '首页' }
+  },
   { path: '/poetry', component: () => import('@/page/poetry/index.vue'), meta: { title: '诗词页' } },
   { path: '/upLoad', component: () => import('@/page/upLoad/index.vue'), meta: { title: '文件上传' } },
   { path: '/course_ware_inquiry', component: () => import('@/page/course_ware_inquiry/index.vue'), meta: { title: '课件查询' } },
